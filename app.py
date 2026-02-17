534ff7f1465198e99f40e27699c9ac84452f056e
IDGS-801-23000572 <85805@alumnos.utleon.edu.mx>
2026-02-16 20:28:09 -0600

    insertar 16/02/2026

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, redirect, render_template, request, url_for
 from flask import flash
 from flask_wtf.csrf import CSRFProtect
 from flask import g
@@ -10,21 +10,39 @@ from models import db, Alumnos
 
 app = Flask(__name__)
 app.config.from_object(DevelopmentConfig)
-csrf = CSRFProtect
+csrf = CSRFProtect()
+
+@app.route("/")
+@app.route("/index")
+def index():
+	create_form=forms.UserForm2(request.form)
+	alumno = Alumnos.query.all()
+	return render_template("index.html",form=create_form,alumno=alumno)
+
+
+@app.route("/alumnos", methods=['GET','POST'])
+def alumnos():
+	create_form=forms.UserForm2(request.form)
+	if request.method== 'POST':
+		alum=Alumnos(nombre=create_form.nombre.data,
+			   		apaterno=create_form.apaterno.data,
+					email=create_form.email.data)
+		db.session.add(alum)
+		db.session.commit()
+		return redirect(url_for("index"))
+	return render_template("alumnos.html")
+
 
 
 @app.errorhandler(404)
 def page_not_found(e):
 	return render_template('404.html')
 
-@app.route("/alumnos")
-def alumnos():
-	return render_template("alumnos.html")
 
-@app.route("/")
-@app.route("/index")
-def index():
-	return render_template("index.html")
 
 if __name__ == '__main__':
+	csrf.init_app(app)
+	db.init_app(app)
+	with app.app_context():
+		db.create_all()
 	app.run(debug=True)
